c8829f2d6647bbc392f9d7b055fb9ce784733507
uhama <harounjean@hotmail.fr>
2026-01-09 11:24:10 +0100

    first commit

---
+++ 8 kyu/001.js
@@ -0,0 +1,10 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+function solution(str){
+  for(let i = 0; i < str.length; i++){
+    
+  }
+}
